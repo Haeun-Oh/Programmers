c124e0759632cfae04fe3f66a1d4ff8d10926d94
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2020-12-26 17:45:27 +0900

    Update Skill_tree.py

--- Skill_tree.py
+++ Skill_tree.py
@@ -2,17 +2,10 @@ def solution(skill, skill_trees):
     answer = 0
     
     for i in skill_trees:
-        vec=[]
-        chk=True
-        for j in range(0, len(i)):
-            if i[j] in skill:
-                vec.append(i[j])
-        k=0
-        while(k!=len(vec)):
-            if skill[k] != vec[k]:
-                chk=False
-                break
-            k+=1
-        if chk==True:
+        vec=''#문자열로
+        for j in i:
+            if j in skill:
+                vec+=j
+        if(vec==skill[:len(vec)]):#
             answer+=1
     return answer
