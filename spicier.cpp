9fd17520a1a0e62b0b0281aaf6aeed8955bb9e55
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-01-29 11:37:33 +0900

    우선순위 큐 더 맵게

---
+++ spicier.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <functional>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>>pq;
+    for(auto vec: scoville)
+        pq.push(vec);
+    while(pq.top()<K){
+        if(pq.size()==1)
+            return -1;
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        pq.push(a+b*2);
+        answer++;
+    }
+    return answer;
+}
