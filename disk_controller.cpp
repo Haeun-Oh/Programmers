3cf8adbbf078d2e0e28e41c4ac59d85338c70c38
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-01-31 16:28:46 +0900

    Update disk_controller.cpp

--- disk_controller.cpp
+++ disk_controller.cpp
@@ -1,35 +1,26 @@
-#include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
-
+#include <algorithm> 
+#include <functional>
 using namespace std;
-
-bool compare(vector<int> a, vector<int> b){
-    return a[1] < b[1];
-}
-
+ 
 int solution(vector<vector<int>> jobs) {
-    int answer = 0;
-    int start = 0; // 현재까지 작업이 진행된 시간
-    int time = 0; // 각각의 작업이 진행되는데 걸린 시간들의 합
-    int size = jobs.size();
-
-    sort(jobs.begin(), jobs.end(), compare); // 소요시간으로 우선 배열
-
-    while(!jobs.empty()){
-        for(int i=0; i<jobs.size(); i++){
-            if(jobs[i][0] <= start) {
-                start += jobs[i][1];
-                time += start - jobs[i][0];
-                jobs.erase(jobs.begin() + i);
-                break;
-            }
-
-            if(i == jobs.size()-1) start++;
+    int answer = 0, j = 0, time = 0;
+    sort(jobs.begin(), jobs.end());
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    while (j < jobs.size() || !pq.empty()) {
+        if (jobs.size() > j && time >= jobs[j][0]) {
+            pq.push(make_pair(jobs[j][1], jobs[j][0]));
+			j++;
+            continue;
         }
+        if (!pq.empty()) {
+            time += pq.top().first;
+            answer += time - pq.top().second;
+            pq.pop();
+        }
+        else
+            time = jobs[j][0];
     }
-
-    answer = time / size;
-    return answer;
+    return answer / jobs.size();
 }
