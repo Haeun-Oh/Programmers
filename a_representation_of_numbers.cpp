c7de51ebc7976dc412c864e80d187f3c13444b0f
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-02-20 09:53:46 +0900

    숫자의 표현

---
+++ a_representation_of_numbers.cpp
@@ -0,0 +1,12 @@
+int solution(int n) {
+    int answer = 0;
+    for(int i=1;i<=n/2;i++){
+        int sum=i;
+        for(int j=i+1;j<=n/2+1&&sum<n;j++){
+            sum+=j;
+            if(sum==n)
+                answer++;
+        }
+    }
+    return ++answer;
+}
