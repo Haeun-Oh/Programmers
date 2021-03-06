b60b476ed82f33a1e54dd060a70c4844ba3235b3
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-03-06 16:42:55 +0900

    도둑질

---
+++ DP/Stealing.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+int dp[1000005];
+
+int solution(vector<int> money) {
+    int answer = 0;
+    dp[0] = money[0];
+    dp[1] = money[0]
+    for(int i=2;i<money.size()-1;i++)
+        dp[i] = max(dp[i-2]+money[i], dp[i-1]);
+    answer=dp[money.size()-2];
+    
+    dp[0] = 0;
+    dp[1] = money[1];
+    for(int i=2;i<money.size();i++)
+        dp[i] = max(dp[i-2]+money[i], dp[i-1]);
+    answer=max(answer, dp[money.size()-1]);
+    
+    dp[0] = 0;
+    dp[1] = 0;
+    for(int i=2;i<money.size();i++)
+        dp[i] = max(dp[i-2]+money[i], dp[i-1]);
+    answer=max(answer, dp[money.size()-1]);
+    return answer;
+}
